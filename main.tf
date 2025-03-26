3def1e5d11449ba3f8ce31b340ed86f70afa2f33
Sanket <Sanketchudmunge@gmail.com>
2025-03-26 18:16:26 +0530

    Error Correction

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "rg" {
+resource "azurerm_storage_account" "stg" {
   name                     = "demostorageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "staging"
